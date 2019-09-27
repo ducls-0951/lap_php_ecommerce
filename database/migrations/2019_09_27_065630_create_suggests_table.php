f1f2fa2d17799a4a4f8b5bcc8a76d1716e2a74b6
ledwc <le.sy.duc@sun-asterisk.com>
2019-09-27 14:07:01 +0700

    Create migration.

---
+++ database/migrations/2019_09_27_065630_create_suggests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuggestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suggests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('product_name');
+            $table->text('description');
+            $table->float('price');
+            $table->string('image');
+            $table->bigInteger('user_id');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suggests');
+    }
+}
