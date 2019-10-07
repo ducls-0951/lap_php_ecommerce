6f6778f9e781244a0d204ea6e15179442ab45bc5
ledwc <le.sy.duc@sun-asterisk.com>
2019-10-07 11:03:01 +0700

    Update layout page.

--- resources/lang/vi/header.php
+++ resources/lang/vi/header.php
@@ -8,4 +8,11 @@ return [
     'shoppingCart' => 'Giỏ hàng',
     'login' => 'Đăng nhâp',
     'register' => 'Đăng ký',
+    'viewCart' => 'Xem giỏ hàng',
+    'checkOut' => 'Đặt hàng',
+    'shop' => 'Cửa hàng',
+    'cart' => 'Giỏ hàng',
+    'suggest' => 'Gợi ý',
+    'img' => 'IMG',
+    'icon' => 'ICON',
 ];
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,28 +1,224 @@
-<header>
-    <nav class="navbar navbar-expand-lg nav-light bg-light">
-        <div class="container">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item"><a href=# class="navbar-brand text-dark"><i class="fas fa-store pr-2"></i>{{ __('header.shoesStore') }}</a></li>
-            </ul>
-            <button class="navbar-toggler btn btn-btn-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto text-dark">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-dark"><i class="fas fa-lightbulb pr-2"></i>{{ __('header.suggest') }}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-dark"><i class="fas fa-shopping-cart pr-2"></i>{{ __('header.shoppingCart') }}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-dark"><i class="fas fa-user pr-2"></i>{{ __('header.login') }}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link text-dark"><i class="fas fa-user-edit pr-2"></i>{{ __('header.register') }}</a>
-                    </li>
-                </ul>
+<header class="header2">
+    <!-- Header desktop -->
+    <div class="container-menu-header-v2 p-t-26">
+        <div class="topbar2">
+            <!-- Logo2 -->
+            <a href="#" class="logo2">
+                <h3 class="text-uppercase font-weight-bold">{{ __('header.shoesStore') }}</h3>
+            </a>
+
+            <div class="topbar-child2">
+                <span class="topbar-email">
+                    {{--email user--}}
+                </span>
+
+                <div class="topbar-language rs1-select2">
+                    {{--change language--}}
+                </div>
+
+                <a href="#" class="header-wrapicon1 dis-block m-l-30">
+                    <span class=" h4 lnr lnr-enter"></span>
+                </a>
+                <a href="#" class="header-wrapicon1 dis-block m-l-30">
+                    <span class="h4 lnr lnr-user"></span>
+                </a>
+
+                <span class="linedivide1"></span>
+
+                <div class="header-wrapicon2 m-r-13">
+                    {{-- Cart --}}
+                    <img src="bower_components/template-fesha/images/icons/icon-header-02.png" class="header-icon1 js-show-header-dropdown" alt="ICON">
+                    <span class="header-icons-noti">{{--total product--}}</span>
+
+                    {{-- Header cart noti --}}
+                    <div class="header-cart header-dropdown">
+                        <ul class="header-cart-wrapitem">
+                            <li class="header-cart-item">
+                                {{--image product--}}
+                                <div class="header-cart-item-img">
+                                    <img src="bower_components/template-fesha/images/item-cart-01.jpg" alt="IMG">
+                                </div>
+                                {{--name, price, quantity product--}}
+                                <div class="header-cart-item-txt">
+                                    <a href="#" class="header-cart-item-name">
+                                        {{--name--}}
+                                    </a>
+                                    <span class="header-cart-item-info">
+                                        {{--quantity, price--}}
+                                    </span>
+                                </div>
+                            </li>
+                        </ul>
+
+                        <div class="header-cart-total">
+                            {{--total price--}}
+                        </div>
+
+                        <div class="header-cart-buttons">
+                            <div class="header-cart-wrapbtn">
+                                <!-- Button -->
+                                <a href="#" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">
+                                    {{ __('header.viewCart') }}
+                                </a>
+                            </div>
+
+                            <div class="header-cart-wrapbtn">
+                                <!-- Button -->
+                                <a href="#" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">
+                                    {{ __('header.checkOut') }}
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </nav>
+
+        <div class="wrap_header">
+            <!-- Menu -->
+            <div class="wrap_menu">
+                <nav class="menu">
+                    <ul class="main_menu">
+                        <li>
+                            <a href="#">{{--category product--}}</a>
+                            <ul class="sub_menu">
+                                <li><a href="#">{{--sub category--}}</a></li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="#">{{ __('header.shop') }}</a>
+                        </li>
+
+                        <li>
+                            <a href="#">{{ __('header.cart') }}</a>
+                        </li>
+
+                        <li>
+                            <a href="#">{{ __('header.suggest') }}</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+            <!-- Header Icon -->
+            <div class="header-icons">
+            </div>
+        </div>
+    </div>
+
+    <!-- Header Mobile -->
+    <div class="wrap_header_mobile">
+        <!-- Logo moblie -->
+        <a href="#" class="logo-mobile">
+            <h3 class="text-uppercase font-weight-bold">{{ __('header.shoesStore') }}</h3>
+        </a>
+
+        <!-- Button show menu -->
+        <div class="btn-show-menu">
+            <!-- Header Icon mobile -->
+            <div class="header-icons-mobile">
+                <a href="#" class="header-wrapicon1 dis-block m-l-30">
+                    <span class=" h4 lnr lnr-enter"></span>
+                </a>
+                <a href="#" class="header-wrapicon1 dis-block m-l-30">
+                    <span class="h4 lnr lnr-user"></span>
+                </a>
+
+                <span class="linedivide2"></span>
+
+                <div class="header-wrapicon2">
+                    <img src="bower_components/template-fesha/images/icons/icon-header-02.png" class="header-icon1 js-show-header-dropdown" alt="{{ __('header.icon') }}">
+                    <span class="header-icons-noti">0</span>
+
+                    <!-- Header cart noti -->
+                    <div class="header-cart header-dropdown">
+                        <ul class="header-cart-wrapitem">
+                            <li class="header-cart-item">
+                                {{--image product--}}
+                                <div class="header-cart-item-img">
+                                    <img src="bower_components/template-fesha/images/item-cart-01.jpg" alt="{{ __('header.img') }}">
+                                </div>
+
+                                <div class="header-cart-item-txt">
+                                    <a href="#" class="header-cart-item-name">
+                                        {{--name product--}}
+                                    </a>
+
+                                    <span class="header-cart-item-info">
+                                        {{--price, quantity--}}
+                                    </span>
+                                </div>
+                            </li>
+                        </ul>
+
+                        <div class="header-cart-total">
+                            {{--total price--}}
+                        </div>
+
+                        <div class="header-cart-buttons">
+                            <div class="header-cart-wrapbtn">
+                                <!-- Button -->
+                                <a href="#" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">
+                                    {{ __('header.viewCart') }}
+                                </a>
+                            </div>
+
+                            <div class="header-cart-wrapbtn">
+                                <!-- Button -->
+                                <a href="#" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">
+                                    {{ __('header.checkOut') }}
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="btn-show-menu-mobile hamburger hamburger--squeeze">
+                <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <!-- Menu Mobile -->
+    <div class="wrap-side-menu" >
+        <nav class="side-menu">
+            <ul class="main-menu">
+                <li class="item-topbar-mobile p-l-20 p-t-8 p-b-8">
+                    <div class="topbar-child2-mobile">
+                        <span class="topbar-email">
+                        </span>
+
+                        <div class="topbar-language rs1-select2">
+                        </div>
+                    </div>
+                </li>
+
+                <li class="item-menu-mobile">
+                    <a href="#"></a>
+                    <ul class="sub-menu">
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                    </ul>
+                    <i class="arrow-main-menu fa fa-angle-right" aria-hidden="true"></i>
+                </li>
+
+                <li class="item-menu-mobile">
+                    <a href="#">{{ __('header.shop') }}</a>
+                </li>
+
+                <li class="item-menu-mobile">
+                    <a href="#">{{ __('header.cart') }}</a>
+                </li>
+
+                <li class="item-menu-mobile">
+                    <a href="#">{{ __('header.suggest') }}</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </header>
